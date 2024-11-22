7c478b71f9821ec81d7bc8380f4997345c6076c5
Makbelhailu <mcwelson92@gmail.com>
2024-11-22 23:13:22 +0300

    still for the game limit

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -100,7 +100,7 @@ const addGame = async (req, res) => {
         .status(403)
         .json({ status: false, message: "Can't add Game data" });
 
-    user.limit -= houseWin * cut;
+    user.limit -= houseWin * user.cut;
     await user.save();
 
     res
