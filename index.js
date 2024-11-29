5ae1d996ad6ac2abec0556a58074fbebcd17785b
Makbelhailu <mcwelson92@gmail.com>
2024-11-29 17:48:03 +0300

    getting ready for packaging

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const compression = require("compression");
-require("dotenv").config();
+const path = require("path");
+const os = require("os")
+const { exec } = require("child_process");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const userRouter = require("./routes/user");
 const cartelaRouter = require("./routes/cartela");
@@ -10,30 +12,33 @@ const gameRouter = require("./routes/game");
 
 const app = express();
 
-// app.use(compression());
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/user", userRouter);
 app.use("/cartela", cartelaRouter);
 app.use("/game", gameRouter);
 app.get("/", (req, res) => {
-  res.send("hi, the server is working");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const uri = process.env.MONGO_URI;
 mongoose
-  .connect(uri, {
-    serverApi: { version: "1", strict: true, deprecationErrors: true },
-  })
+  .connect(uri)
   .then(() => {
     const port = process.env.PORT || 5500;
     app.listen(port, () => {
       console.log("server started successfully at port ", port);
+      if (os.platform() === "win32") {
+        exec("start http://localhost:" + port);
+      } else if (os.platform() === "linux") {
+        exec("xdg-open http://localhost:" + port);
+      }
     });
   })
   .catch((e) => {
     console.error("Error connecting to DB", e.message);
   });
 
-module.exports = app;
